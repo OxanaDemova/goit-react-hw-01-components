12dcd28f764b8b44a6e1505b7f26fc0f1fd91b48
OxanaDemova <93287016+OxanaDemova@users.noreply.github.com>
2022-07-10 00:26:08 +0300

    Statistics+FriendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-import user from '../data/user.json'
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
+    <div>
     <Profile
   username={user.username}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-/>
+    />
+  <Statistics
+        title="Upload stats"
+        stats={data}
+    />
+  <FriendList
+        friends={friends}
+    />  
+
+   </div>
 
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,9 @@
-import user from '../../data/user.json';
 import PropTypes from 'prop-types';
 
 export const Profile = ({ avatar, username, tag, location, stats }) => {
  
     return <div className="profile">
-    <div className="description">
+<div className="description">
     <img
       src={avatar}
       alt="User avatar"
@@ -14,8 +13,7 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
     <p className="tag">{tag}</p>
     <p className="location">{location}</p>
   </div>
-
-  <ul className="stats">
+    <ul className="stats">
     <li>
       <span className="label">Followers</span>
       <span className="quantity">{stats.followers}</span>
@@ -28,13 +26,15 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
       <span className="label">Likes</span>
       <span className="quantity">{stats.likes}</span>
     </li>
-  </ul> 
-</div>
-}
+  </ul>
+ 
+    </div>
+};
+
 Profile.propTypes = {
-    avatar: PropTypes.string,
+    avatar: PropTypes.string.isRequired,
     username: PropTypes.string.isRequired,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    stats: PropTypes.objectOf(PropTypes.number)
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    stats: PropTypes.objectOf(PropTypes.number).isRequired
 }
